d50b4f8e52b79b235bdb421369f36be41055e365
unknown <david.vieuxbled@outlook.fr>
2021-12-05 18:09:00 +0100

    add php branch

---
+++ index.php
@@ -0,0 +1,74 @@
+    <?php 
+    include "template/header.php"; 
+    include "template/nav.php";
+    ?>
+  
+  <!-- ###############################################    MAIN   ###################################################### -->
+  <main class="container my-4">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-6">
+        <article class="d-flex border my-2">
+          <section class="w-50 p-2 d-flex flex-column justify-content-between">
+            <h2 class="accountTitle">Compte n°1</h2>
+            <h3>Solde: 2000€</h3>
+          </section>
+          <aside class="w-50">
+            <ul class="list-group text-center">
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Dépôt</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Retrait</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-danger">Supprimer</button></li>
+            </ul>
+          </aside>
+        </article>
+      </div>
+      <div class="col-12 col-md-6 col-lg-6">
+        <article class="d-flex border my-2">
+          <section class="w-50 p-2 d-flex flex-column justify-content-between">
+            <h2 class="accountTitle">Compte n°2</h2>
+            <h3>Solde: 2000€</h3>
+          </section>
+          <aside class="w-50">
+            <ul class="list-group text-center">
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Dépôt</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Retrait</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-danger">Supprimer</button></li>
+            </ul>
+          </aside>
+        </article>
+      </div>
+      <div class="col-12 col-md-6 col-lg-6">
+        <article class="d-flex border my-2">
+          <section class="w-50 p-2 d-flex flex-column justify-content-between">
+            <h2 class="accountTitle">Compte n°3</h2>
+            <h3>Solde: 2000€</h3>
+          </section>
+          <aside class="w-50">
+            <ul class="list-group text-center">
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Dépôt</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Retrait</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-danger">Supprimer</button></li>
+            </ul>
+          </aside>
+        </article>
+      </div>
+      <div class="col-12 col-md-6 col-lg-6">
+        <article class="d-flex border my-2">
+          <section class="w-50 p-2 d-flex flex-column justify-content-between">
+            <h2 class="accountTitle">Compte n°4</h2>
+            <h3 class="mt-4">Solde: 2000€</h3>
+          </section>
+          <aside class="w-50">
+            <ul class="list-group text-center">
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Dépôt</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-secondary">Retrait</button></li>
+              <li class="list-group-item"><button type="button" class="btn btn-danger">Supprimer</button></li>
+            </ul>
+          </aside>
+        </article>
+      </div>
+    </div>
+  </main>
+  
+  <?php 
+    include "template/footer.php"; 
+    ?>
---
+++ template/header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Saltain Banque</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  
+  <!-- ###############################################   LAYER   ###################################################### -->
+  <div id="layer" class="text-white bg-dark h-100 w-100 position-fixed"></div>
+  <section id="infosBlock" class="container bg-light position-fixed p-0">
+    <h2 id="infosTitle" class="col-12 p-2 m-0 bg-danger text-center">chargement.</h2>
+    <p id="infosText" class="m-2">chargement.</p>
+    <button id="infosButton" class="m-2">J'ai compris</button>
+  </section>
+  <!-- ###############################################   HEADER   ###################################################### -->
+  <header class="text-center header-bg py-5">
+    <section class="container">
+      <h1>Saltain Banque</h1>
+      <p>Pour une gestion financière acrobatique!</p>
+    </section>
+  </header>
